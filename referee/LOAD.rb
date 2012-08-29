9b997e17f77f4b5b234790845ff0df74c58227d2
Eriq Augustine <eaugusti@chromium.org>
2012-08-28 18:03:37 -0700

    Removed webui logs and added and ignore for it.

--- referee/LOAD.rb
+++ referee/LOAD.rb
@@ -1,7 +1,12 @@
 Loader::loadDir('./referee/config')
 Loader::loadDir('./referee/util')
 Loader::loadDir('./referee/commands')
+
+# The core Game class needs to be loaded before any specific game.
 require './referee/game.rb'
+
 Loader::loadDir('./referee/games')
 Loader::loadAllInDir('./referee')
+
+# Webui last.
 Loader::loadDir('./referee/webui')
