9c2484de1648d1e0513deb0dfd6952cd1988934b
Eriq Augustine <eaugusti@chromium.org>
2012-09-03 12:00:33 -0700

    Organized bot.rb some.

--- core/bot.rb
+++ core/bot.rb
@@ -1,48 +1,51 @@
-# This class should only be new()'d once.
-# If you need to relad it for some reason, use reload()
+# The base bot behavior.
 class Bot
    include DB
    include TextSplit
    include Levenshtein
+   include Singleton
+
+ public
 
    attr_reader :channels, :users, :rewriteRules, :emailMap
 
-   # This is the currently active bot.
-   # It may actually be something like a ClefableBot.
-   @@instance = nil
+   def handleServerInput(message)
+      message.strip!
+      log(DEBUG, "Server says: #{message}")
 
-   def self.instance
-      return @@instance
-   end
-
-   def initialize()
-      # { channelName => { userName => user } }
-      @channels = Hash.new{|hash, key| hash[key] = Hash.new() }
-
-      # { userName => user }
-      @users = Hash.new()
-
-      # { target => rewrite }
-      @rewriteRules = getRewriteRules()
-
-      @emailMap = Hash.new()
-      initEmailMap()
-
-      @@instance = self
-   end
-
-   # Wrapper for InputQueue.queueMessage()
-   def sendMessage(message, delay = 0)
-      OutputThread.instance.queueMessage(message, delay)
-   end
-
-   def join(channel)
-      sendMessage("JOIN #{channel}")
-      log(INFO, "Joined #{channel}")
-   end
-
-   def whois(nick)
-      sendMessage("WHOIS #{nick}")
+      # PING :<server>
+      if (match = message.match(/^PING\s:(.*)$/))
+         sendMessage("PONG :#{match[1]}")
+      # :<from user>!<from user>@<from address> PRIVMSG <to> :<message>
+      # <to> is usually a channel
+      elsif (match = message.match(/^:([^!]*)!([^@]*)@([^\s]*)\sPRIVMSG\s([^\s]*)\s:(.*)$/))
+         handlePrivmsg(message, match[1], match[4], match[5].strip)
+      # Recieving user names from the server
+      # ones with ops names are prepended with '@'
+      # :<server> 353 <nick> @ <channel> :<user list (space seperated)>
+      elsif (match = message.match(/^:(\S+)\s+353\s+(\S+)\s+@\s+(\S+)\s+:(.*)$/))
+         handleNickList(message, match[4].split(/\s+/), match[3])
+      # :pratchett.freenode.net 311 TEST_BOT eriq_home ~eriq_home c-50-131-15-127.hsd1.ca.comcast.net * :eaugusti@chromium.org
+      # :<requester irc server> 311 <requester name> <target user> ~<target nick (again?)> <address> * :<extra user info>
+      elsif (match = message.match(/^:(\S+)\s+311\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+\*\s+:(.*)$/))
+         handleWhois(message, match[3], match[5], match[6])
+      # :<from user>!<from user>@<from address> JOIN <channel>
+      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sJOIN\s(\S*)$/))
+         handleUserJoin(message, match[1], match[4])
+      # The bot PART'ed
+      # :#{IRC_NICK}!<something like ~#{part of name}_>@<from address> PART <channel>
+      elsif (match = message.match(/^:#{IRC_NICK}!([^@]*)@(\S*)\sPART\s(\S*)\s*$/))
+         handlePart(message, match[3])
+      # :eriq_home!~eriq_home@c-50-131-15-127.hsd1.ca.comcast.net QUIT :Quit: Leaving
+      # :<from user>!<from user>@<from address> QUIT :<reason>
+      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sQUIT\s+:(.*)$/))
+         handleUserQuit(message, match[1], match[4])
+      # :eriq!~eriq@c-50-131-15-127.hsd1.ca.comcast.net PART #eriq_secret
+      # :eriq!~eriq@c-50-131-15-127.hsd1.ca.comcast.net PART #eriq_secret :"Leaving"
+      # :<from user>!<from user>@<from address> PART <channel>
+      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sPART\s(\S*)/))
+         handleUserPart(message, match[1], match[4], match[5])
+      end
    end
 
    # Available options:
@@ -64,152 +67,6 @@ class Bot
       messages.each{|splitMessage|
          sendMessage("PRIVMSG #{channel} :#{splitMessage}", delay)
       }
-   end
-
-   def ensureUser(user, channel, ops)
-      if (!@users.has_key?(user))
-         userInfo = User.new(user, ops)
-         @users[user] = userInfo
-         @channels[channel][user] = userInfo
-      elsif (!@channels[channel].has_key?(user))
-         @channels[channel][user] = @users[user]
-      end
-   end
-
-   def handleServerInput(message)
-      message.strip!
-      log(DEBUG, "Server says: #{message}")
-
-      # PING :<server>
-      if (match = message.match(/^PING\s:(.*)$/))
-         sendMessage("PONG :#{match[1]}")
-      # :<from user>!<from user>@<from address> PRIVMSG <to> :<message>
-      # <to> is usually a channel
-      elsif (match = message.match(/^:([^!]*)!([^@]*)@([^\s]*)\sPRIVMSG\s([^\s]*)\s:(.*)$/))
-         fromUser = match[1]
-         target = match[4]
-         content = match[5].strip
-
-         responseInfo = ResponseInfo.new(self, fromUser, target, @users[fromUser])
-
-         logMessage = true
-         # If sent message is started with "#{IRC_NICK}:" or "#{SHORT_NICK}:" or "#{TRIGGER}"
-         if (commandMatch = content.strip.match(/^((?:#{IRC_NICK}:)|(?:#{SHORT_NICK}:)|(?:#{TRIGGER}))\s*(.+)$/i))
-            logMessage = Command.invoke(responseInfo, commandMatch[2])
-         # If message was sent in a PM
-         elsif (target == IRC_NICK)
-            logMessage = Command.invoke(responseInfo, content)
-         # If it looks like a ping, but the target of the ping is not in the room.
-         elsif (pingMatch = content.match(/^\^?([^\s\^]+)\^?:(\s+.*)?$/))
-            # Note, we cannot be in a PM because of the previous case.
-            pingTarget = pingMatch[1]
-
-            # If channel exists, but the ping target does not, maybe they mistyped.
-            if (@channels.has_key?(target) && !@channels[target][pingTarget])
-               minInfo = minDistanceNoEquals(pingTarget, @channels[target].keys)
-
-               if (minInfo && minInfo[:dist] <= MAX_PING_CORRECTION_DISTANCE)
-                  chat(target, "Automatic Ping Correction -- #{minInfo[:word]}: ^")
-               end
-            end
-         end
-
-         if (logMessage)
-            logChat(fromUser, target, content)
-         end
-      # Recieving user names from the server
-      # ones with ops names are prepended with '@'
-      # :<server> 353 <nick> @ <channel> :<user list (space seperated)>
-      elsif (match = message.match(/^:(\S+)\s+353\s+(\S+)\s+@\s+(\S+)\s+:(.*)$/))
-         users = match[4].split(/\s+/)
-         channel = match[3]
-
-         users.each{|user|
-            user.strip!
-            ops = false
-
-            if (user.start_with?('@'))
-               ops = true
-               user.sub!(/^@/, '')
-            end
-
-            ensureUser(user, channel, ops)
-         }
-      # :pratchett.freenode.net 311 TEST_BOT eriq_home ~eriq_home c-50-131-15-127.hsd1.ca.comcast.net * :eaugusti@chromium.org
-      # :<requester irc server> 311 <requester name> <target user> ~<target nick (again?)> <address> * :<extra user info>
-      elsif (match = message.match(/^:(\S+)\s+311\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+\*\s+:(.*)$/))
-         user = match[3]
-         address = match[5]
-         extraInfo = match[6]
-
-         if (@users[user])
-            @users[user].address = address
-            @users[user].extraInfo = extraInfo
-            Command.userInfo(user, 'WHOIS', nil)
-         else
-            log(WARN, "Got userinfo on an unknown user: #{user}.")
-         end
-      # :<from user>!<from user>@<from address> JOIN <channel>
-      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sJOIN\s(\S*)$/))
-         user = match[1]
-         channel = match[4]
-         ensureUser(user, channel, false)
-         Command.userJoined(self, channel, user)
-         logChat(user, channel, "** JOIN'd #{channel} **")
-      # Clefable PART'ed
-      # :Clefable_BOT!<something like ~Clefable_>@<from address> PART <channel>
-      elsif (match = message.match(/^:#{IRC_NICK}!([^@]*)@(\S*)\sPART\s(\S*)\s*$/))
-         channel = match[3]
-         @channels.delete(channel)
-         
-         logChat(user, channel, "** PART'd #{channel} **")
-      # :eriq_home!~eriq_home@c-50-131-15-127.hsd1.ca.comcast.net QUIT :Quit: Leaving
-      # :<from user>!<from user>@<from address> QUIT :<reason>
-      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sQUIT\s+:(.*)$/))
-         user = match[1]
-         reason = match[4]
-
-         @users.delete(user)
-         @channels.each_value{|channelUsers|
-            channelUsers.delete(user)
-            Command.userLeft(self, ALL_CHANNELS, user, reason)
-         }
-
-         logChat(user, ALL_CHANNELS, "** QUIT'd Resson: #{reason} **")
-      # :eriq!~eriq@c-50-131-15-127.hsd1.ca.comcast.net PART #eriq_secret
-      # :eriq!~eriq@c-50-131-15-127.hsd1.ca.comcast.net PART #eriq_secret :"Leaving"
-      # :<from user>!<from user>@<from address> PART <channel>
-      elsif (match = message.match(/^:([^!]*)!([^@]*)@(\S*)\sPART\s(\S*)/))
-         user = match[1]
-         channel = match[4]
-         reason = match[5]
-
-         @channels[channel].delete(user)
-
-         found = false
-         @channels.each_value{|channelUsers|
-            if (channelUsers.has_key?(user))
-               found = true
-               break
-            end
-         }
-
-         if (!found)
-            @users.delete(user)
-         end
-
-         Command.userLeft(self, channel, user, reason)
-         logChat(user, channel, "** PART'd #{channel}. **")
-      end
-   end
-
-   def handleStdinInput(command)
-      command.strip!
-
-      if (command.length() > 0)
-         log(DEBUG, "Recieved command: #{command}")
-         Command.invoke(ResponseInfo.new(self, CONSOLE, CONSOLE, CONSOLE_USER), command)
-      end
    end
 
    # Check all channels
@@ -242,8 +99,158 @@ class Bot
       sendMessage("MODE #{channel} -o #{user}")
    end
 
+   def handleStdinInput(command)
+      command.strip!
+
+      if (command.length() > 0)
+         log(DEBUG, "Recieved command: #{command}")
+         Command.invoke(ResponseInfo.new(self, CONSOLE, CONSOLE, CONSOLE_USER), command)
+      end
+   end
+
+   def join(channel)
+      sendMessage("JOIN #{channel}")
+      log(INFO, "Joined #{channel}")
+   end
+
+   def whois(nick)
+      sendMessage("WHOIS #{nick}")
+   end
+
+ protected
+
+   def initialize()
+      # { channelName => { userName => user } }
+      @channels = Hash.new{|hash, key| hash[key] = Hash.new() }
+
+      # { userName => user }
+      @users = Hash.new()
+
+      # { target => rewrite }
+      @rewriteRules = getRewriteRules()
+
+      @emailMap = Hash.new()
+      initEmailMap()
+
+      @@instance = self
+   end
+
+   # Wrapper for InputQueue.queueMessage()
+   def sendMessage(message, delay = 0)
+      OutputThread.instance.queueMessage(message, delay)
+   end
+
    # Inform  the bot that it should perform its periodic actions
    def periodicActions
+   end
+
+ private
+
+   def ensureUser(user, channel, ops)
+      if (!@users.has_key?(user))
+         userInfo = User.new(user, ops)
+         @users[user] = userInfo
+         @channels[channel][user] = userInfo
+      elsif (!@channels[channel].has_key?(user))
+         @channels[channel][user] = @users[user]
+      end
+   end
+
+   def handlePrivmsg(fullMessage, fromUser, target, content)
+      responseInfo = ResponseInfo.new(self, fromUser, target, @users[fromUser])
+
+      logMessage = true
+      # If sent message is started with "#{IRC_NICK}:" or "#{SHORT_NICK}:" or "#{TRIGGER}"
+      if (commandMatch = content.strip.match(/^((?:#{IRC_NICK}:)|(?:#{SHORT_NICK}:)|(?:#{TRIGGER}))\s*(.+)$/i))
+         logMessage = Command.invoke(responseInfo, commandMatch[2])
+      # If message was sent in a PM
+      elsif (target == IRC_NICK)
+         logMessage = Command.invoke(responseInfo, content)
+      # If it looks like a ping, but the target of the ping is not in the room.
+      elsif (pingMatch = content.match(/^\^?([^\s\^]+)\^?:(\s+.*)?$/))
+         # Note, we cannot be in a PM because of the previous case.
+         pingTarget = pingMatch[1]
+
+         # If channel exists, but the ping target does not, maybe they mistyped.
+         if (@channels.has_key?(target) && !@channels[target][pingTarget])
+            minInfo = minDistanceNoEquals(pingTarget, @channels[target].keys)
+
+            if (minInfo && minInfo[:dist] <= MAX_PING_CORRECTION_DISTANCE)
+               chat(target, "Automatic Ping Correction -- #{minInfo[:word]}: ^")
+            end
+         end
+      end
+
+      if (logMessage)
+         logChat(fromUser, target, content)
+      end
+   end
+
+   # This list usually comes from a 353.
+   # A 353 is an indication that the server is sending nicks.
+   def handleNickList(fullMessage, nicks, channel)
+      nicks.each{|nick|
+         nick.strip!
+         ops = false
+
+         if (nick.start_with?('@'))
+            ops = true
+            nick.sub!(/^@/, '')
+         end
+
+         ensureUser(nick, channel, ops)
+      }
+   end
+
+   def handleWhois(fullMessage, user, address, extraInfo)
+      if (@users[user])
+         @users[user].address = address
+         @users[user].extraInfo = extraInfo
+         Command.userInfo(user, 'WHOIS', nil)
+      else
+         log(WARN, "Got userinfo on an unknown user: #{user}.")
+      end
+   end
+
+   def handleUserJoin(fullMessage, user, channel)
+      ensureUser(user, channel, false)
+      Command.userJoined(self, channel, user)
+      logChat(user, channel, "** JOIN'd #{channel} **")
+   end
+
+   # This is the bot parting, not a specific user.
+   def handlePart(fullMessage, channel)
+      @channels.delete(channel)
+      logChat(IRC_NICK, channel, "** PART'd #{channel} **")
+   end
+
+   def handleUserQuit(fullMessage, user, reason)
+      @users.delete(user)
+      @channels.each_value{|channelUsers|
+         channelUsers.delete(user)
+         Command.userLeft(self, ALL_CHANNELS, user, reason)
+      }
+
+      logChat(user, ALL_CHANNELS, "** QUIT'd Resson: #{reason} **")
+   end
+
+   def handleUserPart(fullMessage, user, channel, reason)
+      @channels[channel].delete(user)
+
+      found = false
+      @channels.each_value{|channelUsers|
+         if (channelUsers.has_key?(user))
+            found = true
+            break
+         end
+      }
+
+      if (!found)
+         @users.delete(user)
+      end
+
+      Command.userLeft(self, channel, user, reason)
+      logChat(user, channel, "** PART'd #{channel}. **")
    end
 
    def initEmailMap
